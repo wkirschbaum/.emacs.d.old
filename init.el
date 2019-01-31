0912de550cc070cdc5eac7c8eb9e60e848408bd5
Wilhelm Kirschbaum <wkirschbaum@gmail.com>
2019-01-31 06:21:00 +0200

    Don't use vertical buffers anymore

--- init.el
+++ init.el
@@ -92,12 +92,6 @@
         ido-enable-trap-completion t)
   (ido-mode 1))
 
-(use-package ido-vertical-mode
-  :ensure t
-  :config
-  (setq ido-vertical-define-keys 'C-n-and-C-p-only)
-  (ido-vertical-mode 1))
-
 (use-package ido-completing-read+
   :ensure t
   :config
