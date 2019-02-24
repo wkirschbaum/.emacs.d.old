921bdfab9688d6f578cfb9864f4cc7167b66f8b8
Wilhelm Kirschbaum <wkirschbaum@gmail.com>
2019-02-24 07:32:46 +0200

    Do not initialize for emacs 27 and above

--- init.el
+++ init.el
@@ -2,7 +2,8 @@
 
 ;;; Code:
 
-(package-initialize)
+(if (version< emacs-version "27.0")
+    (package-initialize))
 
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")))
