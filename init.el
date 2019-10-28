d8bb9fdc2cd746bf786b95abe45b68330a33262f
Wilhelm Kirschbaum <wkirschbaum@gmail.com>
2019-10-28 10:35:52 +0200

    Restructure modules

--- init.el
+++ init.el
@@ -5,10 +5,34 @@
 
 ;;; Code:
 
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+
 (load "~/.emacs.d/functions.el")
 (load "~/.emacs.d/garbage.el")
-(load "~/.emacs.d/core.el")
-(load "~/.emacs.d/packages.el")
+
+(load "~/.emacs.d/modules/core.el")
+(load "~/.emacs.d/modules/org.el")
+(load "~/.emacs.d/modules/ivy.el")
+(load "~/.emacs.d/modules/gnus.el")
+(load "~/.emacs.d/modules/programming.el")
+(load "~/.emacs.d/modules/git.el")
+(load "~/.emacs.d/modules/candy.el")
+(load "~/.emacs.d/modules/erc.el")
+(load "~/.emacs.d/modules/extra.el")
+(load "~/.emacs.d/modules/themes.el")
 
 ;;; init.el ends here
 (put 'narrow-to-region 'disabled nil)
--- modules/candy.el
+++ modules/candy.el
@@ -6,26 +6,14 @@
 ;;         nyan-bar-length 24)
 ;;   (nyan-mode))
 
-;; (use-package helpful
-;;   :straight t
-;;   :bind(("C-h f" . helpful-callable)
-;;         ("C-h v" . helpful-variable)
-;;         ("C-h k" . helpful-key)
-;;         ("C-c C-d" . helpful-at-point)
-;;         ("C-h C" . helpful-command)
-;;         ("C-h F" . helpful-function))
-;;   :config
-;;   (setq counsel-describe-function-function #'helpful-callable
-;;         counsel-describe-variable-function #'helpful-variable))
-
 ;; (use-package keycast
 ;;   :straight t)
 
-;; (use-package focus
-;;   :straight t)
+(use-package focus
+  :straight t)
 
-;; (use-package define-word
-;;   :straight t)
+(use-package define-word
+  :straight t)
 
 (use-package minions
   :straight t
@@ -40,7 +28,6 @@
   (bind-keys :map dired-mode-map
              ("i" . dired-subtree-insert)
              (";" . dired-subtree-remove)))
-  )
 
 (use-package all-the-icons-dired
   :straight t
